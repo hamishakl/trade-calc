6decee905fa564cd150eeecaf30486bab4a5606a
Hamish Henare <hamishjustin7@gmail.com>
2021-05-06 10:53:18 +1200

    switched branch to revert changes

--- index.js
+++ index.js
@@ -2,27 +2,33 @@
 // const DailyPNLGoal = 0.05; //Enter your PNL percentage goal
 // const portTradePercentage = 0.40; //Enter your port percentage for each trade
 // const tradePercentage = 0.20; //Enter the return percentage of your trades
-const portStartingAmount = document.getElementById('portfolio--input').value; //Enter your portfolio amount
-const dailyPNLGoal = document.getElementById('dailyPNLGoal--input').value; //Enter your PNL percentage goal
-const portTradePercentage = document.getElementById('portPercentage--input').value; //Enter your port percentage for each trade
-const tradePercentage = document.getElementById('tradePercentage--input').value; //Enter the return percentage of your trades
+let portStartingAmount = document.getElementById("portfolio--input").value; //Enter your portfolio amount
+let dailyPNLGoal = document.getElementById("dailyPNLGoal--input").value; //Enter your PNL percentage goal
+let portTradePercentage = document.getElementById("portPercentage--input")
+  .value; //Enter your port percentage for each trade
+let tradePercentage = document.getElementById("tradePercentage--input").value; //Enter the return percentage of your trades
 
-console.log(portStartingAmount)
+console.log(portStartingAmount);
 console.log(dailyPNLGoal);
 console.log(portTradePercentage);
 console.log(tradePercentage);
 
 let val = (a) => {
-  document.getElementById('c').innerHTML = `Port percentage for each trade = ${a}%`
-  console.log(`Port percentage for each trade = ${a}%`)
-}
+  document.getElementById(
+    "c"
+  ).innerHTML = `Port percentage for each trade = ${a}%`;
+  console.log(`Port percentage for each trade = ${a}%`);
+};
 
 var form = document.getElementById("formWrapper");
-function handleForm(event) { event.preventDefault(); } 
-form.addEventListener('submit', handleForm);
+function handleForm(event) {
+  event.preventDefault();
+}
+form.addEventListener("submit", handleForm);
 
-document.getElementById('a').innerHTML = 
-`Starting port = ${portStartingAmount}`
+document.getElementById(
+  "a"
+).innerHTML = `Starting port = ${portStartingAmount}`;
 
 let arr = [];
 
@@ -67,10 +73,38 @@ let trades = (a) => {
     console.log(" ");
     console.log(`PNL actual = $${pnlCalc(trading, portStartingAmount)}`);
     console.log(" ");
-   console.log( `PNL percentage = ${
-      (pnlCalc(trading, portStartingAmount) / portStartingAmount) * 100
-    }%`);
+    console.log(
+      `PNL percentage = ${
+        pnlCalc(trading, portStartingAmount) / portStartingAmount 
+      }%`
+    );
   }
 };
+
+let tradeHTML = () => {
+  let pnlPrecentage = dailyPNLGoal / 100 
+  let portGoal = portStartingAmount * pnlPrecentage
+  let tradePercentage = portTradePercentage / 100
+  let tradeReturn = tradePercentage / 100 + 1
+  console.log(pnlPrecentage + ' perce')
+  console.log(portGoal + ' portgoal')
+  console.log(tradePercentage + ' tradeperc')
+  console.log(tradeReturn + ' tradereturn')
+  let tradeAmount = portStartingAmount * tradePercentage
+  let tradeGain = tradeAmount * tradeReturn
+  let portMinusTrade = portStartingAmount - tradeAmount
+  console.log(portStartingAmount)
+  console.log(portMinusTrade + ' portAfter')
+  let newPort = tradeGain + portMinusTrade
+  console.log(newPort + 'trade gain')
+
+  if (newPort < portGoal) {
+    tradeHTML()
+    console.log(newPort + 'round 1')
+  } else {
+    console.log(newPort)
+  }
+
+}
 
 console.log(trades(portStartingAmount));
